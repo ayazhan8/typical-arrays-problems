2bfa6b1a56465490e967bf65cea8468deaf09cca
ayazhan8 <54767127+ayazhan8@users.noreply.github.com>
2021-03-15 19:23:42 +0600

    Added a solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
-}
+    var min = array[0];
 
-exports.max = function max (array) {
-  return 0;
-}
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] <= min) {
+            min = array[i];
+        }
+    }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    return min;
+};
+
+exports.max = function max(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
+
+    var max = array[0];
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] >= max) {
+            max = array[i];
+        }
+    }
+
+    return max;
+};
+
+exports.avg = function avg(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
+
+    var avg = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        avg += array[i];
+    }
+
+    avg /= array.length;
+
+    return avg;
+};
